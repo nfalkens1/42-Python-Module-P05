ed948b528d79458c3050248682709f35dd481d41
Niklas Falkenstein <nfalkens@3-A-10.42heilbronn.de>
2026-08-10 16:25:26 +0200

    ex1 complete with mypy and flake8 checked

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -4,8 +4,13 @@ from abc import ABC, abstractmethod
 
 class DataProcessor(ABC):
     """
-    description goes here
+    Parent class that defines abstractmethods validate and ingest
+    Skips over abstractmethods
+    Produces output by popping data at index 0 + the respected rank
     """
+    data: list[str]
+    rank: int
+
     @abstractmethod
     def validate(self, data: Any) -> bool:
         ...
@@ -15,12 +20,18 @@ class DataProcessor(ABC):
         ...
 
     def output(self) -> tuple[int, str]:
-        ...
+        if not self.data:
+            raise IndexError("No data provided")
+        rank = self.rank - len(self.data)
+        data_out = self.data.pop(0)
+        return (rank, data_out)
 
 
 class NumericProcessor(DataProcessor):
     """
-    description goes here
+    Validate: checks if data is int/float or a list of int/float
+    Ingest: raises TypeError if data is not numeric
+    Formats items to str and adds then to the list while incrementing rank
     """
 
     def __init__(self) -> None:
@@ -48,7 +59,8 @@ class NumericProcessor(DataProcessor):
 
 class TextProcessor(DataProcessor):
     """
-    description goes here
+    Validate: Text input data (str or list of str)
+    Ingest: appends Item to data and increments rank
     """
 
     def __init__(self) -> None:
@@ -76,7 +88,8 @@ class TextProcessor(DataProcessor):
 
 class LogProcessor(DataProcessor):
     """
-    descripton goes here
+    Validate: checks if every k/v pair in each dict is of type str
+    Ingest: formats log messages
     """
 
     def __init__(self) -> None:
@@ -106,3 +119,58 @@ class LogProcessor(DataProcessor):
             log_format = f"{data['log_level']}: {data['log_message']}"
             self.data.append(log_format)
             self.rank += 1
+
+
+if __name__ == "__main__":
+    header = "=== Code Nexus - Data Processor ==="
+    print(f"\n{header}\n")
+    print("Testing Numeric Processor...")
+    int_data = 42
+    num_instance = NumericProcessor()
+    validation_1 = num_instance.validate(int_data)
+    str_data = "Hello"
+    validation_2 = num_instance.validate(str_data)
+    print(f" Trying to validate input '{int_data}': {validation_1}")
+    print(f" Trying to validate input '{str_data}': {validation_2}")
+    invalid_ingest = "foo"
+    print(f" Test invalid ingestion of string '{invalid_ingest}'"
+          " without prior validation:")
+    try:
+        num_instance.ingest(invalid_ingest)
+    except TypeError as e:
+        print(f" Got exception: {e}")
+    valid_data: list[int | float] = [1, 2, 3, 4, 5]
+    print(f" Processing data: {valid_data}")
+    if num_instance.validate(valid_data):
+        num_instance.ingest(valid_data)
+        print(" Extracting 3 values")
+        for i in range(3):
+            num_out = num_instance.output()
+            print(f" Numeric value {num_out[0]}: {num_out[1]}")
+    print()
+    print("Testing Text Processor...")
+    text_instance = TextProcessor()
+    validation_3 = text_instance.validate(int_data)
+    print(f" Trying to validate input '{int_data}': {validation_3}")
+    text_data = ["Hello", "Nexus", "World"]
+    print(f" Processing data: {text_data}")
+    if text_instance.validate(text_data):
+        text_instance.ingest(text_data)
+        print(" Extracting 1 value...")
+        text_out = text_instance.output()
+        print(f" Text value {text_out[0]}: {text_out[1]}")
+
+    print()
+    print("Testing Log Processor...")
+    log_instance = LogProcessor()
+    validation_4 = log_instance.validate(str_data)
+    print(f" Trying to validate input '{str_data}': {validation_4}")
+    log_list = [{"log_level": "NOTICE", "log_message": "Connection to server"},
+                {"log_level": "ERROR", "log_message": "Unauthorized access!!"}]
+    print(f" Processing data: {log_list}")
+    if log_instance.validate(log_list):
+        log_instance.ingest(log_list)
+        print(" Extracting 2 values...")
+        for i in range(2):
+            log_out = log_instance.output()
+            print(f" Log entry {log_out[0]}: {log_out[1]}")
